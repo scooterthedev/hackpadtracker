a23ed299755d420707d877dae7420f116c708852
Scooter <info@talkufy.com>
2024-10-31 15:58:21 -0400

    added v1.2.6 for the db
    Signed-off-by: Scooter <info@talkufy.com>

---
+++ src/build.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+const srcDir = path.join(__dirname, 'src');
+const destDir = path.join(__dirname, 'dist');
+
+function copyFiles(src, dest) {
+  if (!fs.existsSync(dest)) {
+    fs.mkdirSync(dest, { recursive: true });
+  }
+
+  fs.readdirSync(src).forEach((file) => {
+    const srcFile = path.join(src, file);
+    const destFile = path.join(dest, file);
+
+    if (fs.lstatSync(srcFile).isDirectory()) {
+      copyFiles(srcFile, destFile);
+    } else {
+      fs.copyFileSync(srcFile, destFile);
+    }
+  });
+}
+
+// Copy JavaScript files
+copyFiles(srcDir, destDir);
