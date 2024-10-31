e3edc2d8d73ef45fb2427540cbe20518d603985b
Scooter <info@talkufy.com>
2024-10-30 23:24:06 -0400

    fixed token leak

--- src/App.tsx
+++ src/App.tsx
@@ -108,7 +108,7 @@ useEffect(() => {
           },
           body: new URLSearchParams({
             client_id: '2210535565.7957522136834',
-            client_secret: "73271bfeae65d8281541c9802507f364",
+            client_secret: process.env.client_secret,
             code,
             redirect_uri: 'https://hackpad-progress-tracker.vercel.app/callback',
           }),
